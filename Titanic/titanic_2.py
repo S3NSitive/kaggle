418e718bdb4a6a1bc2f342718a759e3765d03177
swkim <swkim0512@gmail.com>
2018-11-09 15:56:54 +0900

    Update Source

--- Titanic/titanic_2.py
+++ Titanic/titanic_2.py
@@ -1,20 +1,26 @@
+import warnings
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import warnings
-
 # importing all the required ML packages
-from sklearn.linear_model import LogisticRegression  # logistic regression
 from sklearn import svm  # support vector machine
-from sklearn.ensemble import RandomForestClassifier  # Random forest
-from sklearn.neighbors import KNeighborsClassifier  # KNN
-from sklearn.naive_bayes import GaussianNB  # Naive bayes
-from sklearn.tree import DecisionTreeClassifier  # Decision Tree
-from sklearn.model_selection import train_test_split  # training and testing data split
 from sklearn import metrics  # accuracy measure
+from sklearn.naive_bayes import GaussianNB  # Naive bayes
 from sklearn.metrics import confusion_matrix  # for confusion matrix
+from sklearn.tree import DecisionTreeClassifier  # Decision Tree
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier  # KNN
+from sklearn.ensemble import RandomForestClassifier  # Random forest
+from sklearn.linear_model import LogisticRegression  # logistic regression
+from sklearn.model_selection import train_test_split  # training and testing data split
+
+# Cross Validation
+from sklearn.model_selection import KFold  # for K-fold cross validation
+from sklearn.model_selection import cross_val_score  # score evaluation
+from sklearn.model_selection import cross_val_predict  # prediction
+
 warnings.filterwarnings('ignore')
 plt.style.use('fivethirtyeight')
 
@@ -231,7 +237,7 @@ fig = plt.gcf()
 fig.set_size_inches(18, 15)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
-# plt.show()
+plt.show()
 
 # Predictive Modeling
 train, test = train_test_split(data, test_size=0.3, random_state=0, stratify=data['Survived'])
@@ -242,12 +248,100 @@ test_Y = test[test.columns[:1]]
 X = data[data.columns[1:]]
 Y = data['Survived']
 
+# Radial Support Vector Machines(rbf-SVM)
 model = svm.SVC(kernel='rbf', C=1, gamma=0.1)
 model.fit(train_X, train_Y)
 prediction1 = model.predict(test_X)
 print(f"Accuracy for rbf SVM is {metrics.accuracy_score(prediction1, test_Y):.3f}")
 
+# Linear Support Vector Machine(linear-SVM)
 model = svm.SVC(kernel='linear', C=0.1, gamma=0.1)
 model.fit(train_X, train_Y)
 prediction2 = model.predict(test_X)
 print(f"Accuracy for linear SVM is {metrics.accuracy_score(prediction2, test_Y):.3f}")
+
+# Logistic Regression
+model = LogisticRegression()
+model.fit(train_X, train_Y)
+prediction3 = model.predict(test_X)
+print(f"The accuracy of the Logistic Regression is {metrics.accuracy_score(prediction3, test_Y):.3f}")
+
+# Decision Tree
+model = DecisionTreeClassifier()
+model.fit(train_X, train_Y)
+prediction4 = model.predict(test_X)
+print(f"The accuracy of the Decision Tree is {metrics.accuracy_score(prediction4, test_Y):.3f}")
+
+# K-Nearest Neighbours(KNN)
+model = KNeighborsClassifier()
+model.fit(train_X, train_Y)
+prediction5 = model.predict(test_X)
+print(f"The accuracy of the KNN is {metrics.accuracy_score(prediction5, test_Y):.3f}")
+
+a_index = list(range(1, 11))
+a = pd.Series()
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in list(range(1, 11)):
+    model = KNeighborsClassifier(n_neighbors=i)
+    model.fit(train_X, train_Y)
+    prediction = model.predict(test_X)
+    a = a.append(pd.Series(metrics.accuracy_score(prediction, test_Y)))
+
+plt.plot(a_index, a)
+plt.xticks(x)
+fig = plt.gcf()
+fig.set_size_inches(12, 6)
+plt.show()
+print(f"Accuracies for different values of n are: {a.values} with the max value as {a.values.max()}")
+
+# Gaussian Naive Bayes
+model = GaussianNB()
+model.fit(train_X, train_Y)
+prediction6 = model.predict(test_X)
+print(f"The accuracy of the NaiveBayes is {metrics.accuracy_score(prediction6, test_Y):.3f}")
+
+# Random Forests
+model = RandomForestClassifier(n_estimators=100)
+model.fit(train_X, train_Y)
+prediction7 = model.predict(test_X)
+print(f"The accuracy of the Random Forest is {metrics.accuracy_score(prediction7, test_Y):.3f}")
+
+# Cross Validation
+kfold = KFold(n_splits=10, random_state=22)  # k=10, split the data into 10 equal parts
+xyz = []
+accuracy = []
+std = []
+classifiers = ['Linear Svm', 'Radial Svm', 'Logistic Regression', 'KNN', 'Decision Tree', 'Naive Bayes',
+               'Random Forest']
+models=[svm.SVC(kernel='linear'), svm.SVC(kernel='rbf'), LogisticRegression(), KNeighborsClassifier(n_neighbors=9),
+        DecisionTreeClassifier(), GaussianNB(), RandomForestClassifier(n_estimators=100)]
+for i in models:
+    model = i
+    cv_result = cross_val_score(model, X, Y, cv=kfold, scoring="accuracy")
+    cv_result = cv_result
+    xyz.append(cv_result.mean())
+    std.append(cv_result.std())
+    accuracy.append(cv_result)
+new_models_dataframe2 = pd.DataFrame({'CV Mean': xyz, 'Std': std}, index=classifiers)
+print(new_models_dataframe2)
+
+plt.subplots(figsize=(18, 6))
+box = pd.DataFrame(accuracy, index=[classifiers])
+box.T.boxplot()
+plt.show()
+
+new_models_dataframe2['CV Mean'].plot.barh(width=0.8)
+plt.title('Average CV Mean Accuracy')
+fig = plt.gcf()
+fig.set_size_inches(8, 5)
+plt.show()
+
+# Hyper-Parameters Tuning
+C = [0.05, 0.1, 0.2, 0.3, 0.25, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+gamma = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+kernel = ['rbf', 'linear']
+hyper = {'kernel': kernel, 'C': C, 'gamma': gamma}
+gd = GridSearchCV(estimator=svm.SVC(), param_grid=hyper, verbose=True)
+gd.fit(X, Y)
+print(gd.best_score_)
+print(gd.best_estimator_)
