8d5e6ad61514bbe554b6347f6a76282171080cc9
swkim <swkim0512@gmail.com>
2018-11-19 00:05:24 +0900

    Update Source

--- Titanic/mytitanic.py
+++ Titanic/mytitanic.py
@@ -6,21 +6,26 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # importing all the required ML packages
-from sklearn import svm  # support vector machine
+from sklearn.svm import SVC  # support vector machine
 from sklearn import metrics  # accuracy measure
 from sklearn.naive_bayes import GaussianNB  # Naive bayes
 from sklearn.metrics import confusion_matrix  # for confusion matrix
 from sklearn.tree import DecisionTreeClassifier  # Decision Tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.neighbors import KNeighborsClassifier  # KNN
 from sklearn.ensemble import RandomForestClassifier  # Random forest
 from sklearn.linear_model import LogisticRegression  # logistic regression
 from sklearn.model_selection import train_test_split  # training and testing data split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 # Cross Validation
 from sklearn.model_selection import KFold  # for K-fold cross validation
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score  # score evaluation
 from sklearn.model_selection import cross_val_predict  # prediction
+from sklearn.model_selection import StratifiedShuffleSplit
 
 # Ensembling
 import xgboost as xg  # xgboost
@@ -37,8 +42,8 @@ PassengerId = test['PassengerId']
 
 # print(train.shape[0])
 # print(test.shape[0])
-print(train.columns)
-print(train.isnull().sum())
+# print(train.columns)
+# print(train.isnull().sum())
 
 # Sex
 sex_mapping = {'male': 0, 'female': 1}
@@ -77,7 +82,7 @@ test['Initial'].replace(['Mlle', 'Mme', 'Ms', 'Dr', 'Major', 'Lady', 'Countess',
                          'Other', 'Other', 'Other', 'Mr', 'Mr', 'Mr', 'Mr'], inplace=True)
 
 dataset = pd.concat([train, test], axis=0)
-print(dataset.groupby('Initial').mean())
+# print(dataset.groupby('Initial').mean())
 
 train.loc[(train['Age'].isnull()) & (train['Initial'] == 'Master'), 'Age'] = 5
 train.loc[(train['Age'].isnull()) & (train['Initial'] == 'Miss'), 'Age'] = 22
@@ -157,5 +162,175 @@ test = pd.get_dummies(test, columns=['Pclass'], prefix='Pclass')
 
 # drop
 drop_category = ['SibSp', 'Parch', 'Name', 'PassengerId', 'Age']
-train.drop('SibSp', 'Parch', 'Name', 'PassengerId', 'Age')
-print(train.columns)
+train.drop(labels=drop_category, axis=1, inplace=True)
+# print(train.columns)
+
+# Modeling
+y_train = train['Survived']
+X_train = train.drop(labels=['Survived'], axis=1)
+random_state = 2
+kfold = StratifiedKFold(n_splits=5, shuffle=True)
+
+classifiers = []
+classifiers.append(SVC(random_state=random_state))
+classifiers.append(MLPClassifier(random_state=random_state))
+classifiers.append(LogisticRegression(random_state=random_state))
+classifiers.append(ExtraTreesClassifier(random_state=random_state))
+classifiers.append(KNeighborsClassifier())
+classifiers.append(DecisionTreeClassifier(random_state=random_state))
+classifiers.append(RandomForestClassifier(random_state=random_state))
+classifiers.append(LinearDiscriminantAnalysis())
+classifiers.append(AdaBoostClassifier(DecisionTreeClassifier(random_state=random_state),
+                                      random_state=random_state, learning_rate=0.1))
+classifiers.append(GradientBoostingClassifier(random_state=random_state))
+classifiers.append(xg.XGBClassifier(random_state=random_state))
+
+cv_results = []
+for classifier in classifiers:
+    cv_results.append(cross_val_score(classifier, X_train, y=y_train, scoring='accuracy', cv=kfold, n_jobs=4))
+
+cv_means = []
+cv_std = []
+cv_accuracy = []
+for cv_result in cv_results:
+    cv_means.append(cv_result.mean())
+    cv_std.append(cv_result.std())
+    cv_accuracy.append(cv_result)
+
+cv_res = pd.DataFrame({'CrossValMeans': cv_means, 'CrossValStd': cv_std,
+                       'Algorithm': ['SVC', 'MLP', 'LogisticRegression', 'ExtraTrees', 'KNN',
+                                     'DecisionTree', 'RandomForest', 'LinearDiscriminant',
+                                     'AdaBoost', 'GradientBoost', 'XGBoost']})
+g = sns.barplot('CrossValMeans', 'Algorithm', data=cv_res)
+g.set_title('Cross Validation Scores')
+g.set_xlabel('Mean Accuracy')
+print(cv_res)
+# plt.show()
+
+# Hyperparameter Turning
+C = [1, 50, 100, 500, 1000]
+gamma = [0.001, 0.1, 0.2, 1]
+subsample = [0.5, 0.7, 0.9]
+colsample_bytree = [0.5, 0.7, 0.9]
+min_child_weight = [1, 2, 3, 5]
+max_depth = [3, 4, 8]
+max_features = [2, 3, 10]
+min_samples_split = [2, 3, 10]
+min_samples_leaf = [2, 3, 10]
+n_estimators = list(range(300, 900, 100))
+learning_rate = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
+
+best_estimators = []
+best_scores = []
+
+SVMC = SVC(probability=True)
+SVMC_params = {'kernel': ['rbf'],
+               'gamma': gamma,
+               'C': C}
+gsSVMC = GridSearchCV(SVMC, param_grid=SVMC_params, cv=kfold, scoring='accuracy', verbose=1, n_jobs=4)
+gsSVMC.fit(X_train, y_train)
+SVMC_best_estimator = gsSVMC.best_estimator_
+SVMC_best_score = gsSVMC.best_score_
+best_estimators.append(SVMC_best_estimator)
+best_scores.append(SVMC_best_score)
+print('SVMC Clear')
+
+ExtC = ExtraTreesClassifier()
+ExtC_params = {'max_depth': [None],
+               'max_features': max_features,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': [False],
+               'n_estimators': n_estimators,
+               'criterion': ['gini']}
+
+gsExtC = GridSearchCV(ExtC, param_grid=ExtC_params, cv=kfold, scoring='accuracy', verbose=1, n_jobs=4)
+gsExtC.fit(X_train, y_train)
+ExtC_best_estimator = gsExtC.best_estimator_
+ExtC_best_score = gsExtC.best_score_
+best_estimators.append(ExtC_best_estimator)
+best_scores.append(ExtC_best_score)
+print('ExtC Clear')
+
+RFC = RandomForestClassifier()
+RFC_params = {'max_depth': [None],
+              'max_features': max_features,
+              'min_samples_split': min_samples_split,
+              'min_samples_leaf': min_samples_leaf,
+              'bootstrap': [False],
+              'n_estimators': n_estimators,
+              'criterion': ['gini']}
+
+gsRFC = GridSearchCV(RFC, param_grid=RFC_params, cv=kfold, scoring='accuracy', verbose=1, n_jobs=4)
+gsRFC.fit(X_train, y_train)
+RFC_best_estimator = gsRFC.best_estimator_
+RFC_best_score = gsRFC.best_score_
+best_estimators.append(RFC_best_estimator)
+best_scores.append(RFC_best_score)
+print('RFC Clear')
+
+DTC = DecisionTreeClassifier()
+AdaDTC = AdaBoostClassifier(DTC, random_state=random_state)
+Ada_params = {'base_estimator__criterion': ['gini', 'entropy'],
+              'base_estimator__splitter': ['best', 'random'],
+              'algorithm': ['SAMME', 'SAMME.R'],
+              'n_estimators': n_estimators,
+              'learning_rate': learning_rate}
+
+gsAdaDTC = GridSearchCV(AdaDTC, param_grid=Ada_params, cv=kfold, scoring='accuracy', verbose=1, n_jobs=4)
+gsAdaDTC.fit(X_train, y_train)
+Ada_best_estimator = gsAdaDTC.best_estimator_
+Ada_best_score = gsAdaDTC.best_score_
+best_estimators.append(Ada_best_estimator)
+best_scores.append(Ada_best_score)
+print('Ada Clear')
+
+GBC = GradientBoostingClassifier()
+GBC_params = {'loss': ['deviance'],
+              'max_depth': max_depth,
+              'min_samples_leaf': min_samples_leaf,
+              'max_features': max_features,
+              'n_estimators': n_estimators,
+              'learning_rate': learning_rate}
+
+gsGBC = GridSearchCV(GBC, param_grid=GBC_params, cv=kfold, scoring='accuracy', verbose=1, n_jobs=4)
+gsGBC.fit(X_train, y_train)
+GBC_best_estimator = gsGBC.best_estimator_
+GBC_best_score = gsGBC.best_score_
+best_estimators.append(GBC_best_estimator)
+best_scores.append(GBC_best_score)
+print('GBC Clear')
+
+XGBC = xg.XGBClassifier(objective='binary:logistic')
+XGBC_params = {'max_depth': max_depth,
+               'gamma': gamma,
+               # 'subsample': subsample,
+               'min_child_weight': min_child_weight,
+               'n_estimators': n_estimators,
+               'learning_rate': learning_rate}
+
+gsXGBC = GridSearchCV(XGBC, param_grid=XGBC_params, cv=kfold, scoring='accuracy', verbose=1, n_jobs=4)
+gsXGBC.fit(X_train, y_train)
+XGBC_best_estimator = gsXGBC.best_estimator_
+XGBC_best_score = gsXGBC.best_score_
+best_estimators.append(XGBC_best_estimator)
+best_scores.append(XGBC_best_score)
+print('XGBC Clear')
+
+tuning_res = pd.DataFrame({'best_estimator': best_estimators, 'best_score': best_scores,
+                            'Algorithm': ['SVC', 'ExtraTreesClassifier', 'RandomForest',
+                                          'AdaBoost', 'GradientBoost', 'XGBoost']})
+print(tuning_res)
+
+votingC = VotingClassifier(estimators=[('RFC', RFC_best_estimator),
+                                       ('ExtC', ExtC_best_estimator),
+                                       ('SVMC', SVMC_best_estimator),
+                                       ('Ada', Ada_best_estimator),
+                                       ('GBC', GBC_best_estimator),
+                                       ('XGBC', XGBC_best_estimator)],
+                           voting='soft')
+votingC = votingC.fit(X_train, y_train)
+
+test_Survived = pd.Series(votingC.predict(test), name='Survived')
+results = pd.concat([PassengerId, test_Survived], axis=1)
+results.to_csv('./data/my_titanic_python_voting.csv', index=False)
