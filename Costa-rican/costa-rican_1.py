cbbd52a969290a7d6c1d75bda64305f619a01abd
swkim <swkim0512@gmail.com>
2018-12-05 20:14:24 +0900

    Look at Summary Information
    Look at Summary Information from float columns

--- Costa-rican/costa-rican_1.py
+++ Costa-rican/costa-rican_1.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from collections import OrderedDict
+
 plt.style.use("fivethirtyeight")
 plt.rcParams["font.size"] = 18
 plt.rcParams["patch.edgecolor"] = "k"
@@ -16,7 +18,8 @@ test = pd.read_csv("data/test.csv")
 
 # print(train.info())
 
-print(train.select_dtypes(np.int64).nunique().value_counts().sort_index())
+# Integer Columns
+# print(train.select_dtypes(np.int64).nunique().value_counts().sort_index())
 train.select_dtypes(np.int64).nunique().value_counts().sort_index().plot.bar(color="blue",
                                                                              figsize=(12, 6),
                                                                              edgecolor="k",
@@ -26,3 +29,28 @@ plt.ylabel("Count")
 plt.title("Count of unique values in integer columns")
 plt.show()
 
+# Float Columns
+plt.figure(figsize=(20, 16))
+plt.style.use("fivethirtyeight")
+
+# Color mapping
+colors = OrderedDict({1: "red", 2: "orange", 3: "blue", 4: "green"})
+poverty_mapping = OrderedDict({1: "extreme", 2: "moderate", 3: "vulnerable", 4: "non vulnerable"})
+
+print(train.select_dtypes(np.float))
+# Iterate through the float columns
+for i, col in enumerate(train.select_dtypes("float")):
+    ax = plt.subplot(4, 2, i+1)
+    for poverty_level, color in colors.items():
+        sns.kdeplot(train.loc[train["Target"] == poverty_level, col].dropna(), ax=ax, color=color,
+                    label=poverty_mapping[poverty_level])
+
+    plt.title(f"{col.capitalize()} Distribution")
+    plt.xlabel(f"{col}")
+    plt.ylabel("Density")
+
+plt.subplots_adjust(top=2)
+plt.show()
+
+# Object Columns
+print(train.select_dtypes("object").head())
