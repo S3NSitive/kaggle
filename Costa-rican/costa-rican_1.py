a1badfd6dc853200559356faadf0b5cc835d0fa0
swkim <swkim0512@gmail.com>
2018-12-06 17:56:15 +0900

    From Exploring Label Distribution to Missing Variables and
    The distribution of labels and Wrong Labels check

--- Costa-rican/costa-rican_1.py
+++ Costa-rican/costa-rican_1.py
@@ -27,7 +27,7 @@ train.select_dtypes(np.int64).nunique().value_counts().sort_index().plot.bar(col
 plt.xlabel("Number of Unique values")
 plt.ylabel("Count")
 plt.title("Count of unique values in integer columns")
-plt.show()
+# plt.show()
 
 # Float Columns
 plt.figure(figsize=(20, 16))
@@ -37,10 +37,9 @@ plt.style.use("fivethirtyeight")
 colors = OrderedDict({1: "red", 2: "orange", 3: "blue", 4: "green"})
 poverty_mapping = OrderedDict({1: "extreme", 2: "moderate", 3: "vulnerable", 4: "non vulnerable"})
 
-print(train.select_dtypes(np.float))
 # Iterate through the float columns
 for i, col in enumerate(train.select_dtypes("float")):
-    ax = plt.subplot(4, 2, i+1)
+    ax = plt.subplot(4, 2, i + 1)
     for poverty_level, color in colors.items():
         sns.kdeplot(train.loc[train["Target"] == poverty_level, col].dropna(), ax=ax, color=color,
                     label=poverty_mapping[poverty_level])
@@ -50,7 +49,70 @@ for i, col in enumerate(train.select_dtypes("float")):
     plt.ylabel("Density")
 
 plt.subplots_adjust(top=2)
-plt.show()
+# plt.show()
 
 # Object Columns
-print(train.select_dtypes("object").head())
+# print(train.select_dtypes("object").head())
+
+mapping = {"yes": 1, "no": 0}
+
+for df in [train, test]:
+    df["dependency"] = df["dependency"].replace(mapping).astype(np.float64)
+    df["edjefa"] = df["edjefa"].replace(mapping).astype(np.float64)
+    df["edjefe"] = df["edjefe"].replace(mapping).astype(np.float64)
+
+# print(train[["dependency", "edjefa", "edjefe"]].describe())
+
+plt.figure(figsize=(16, 12))
+
+# Iterate through the float columns
+for i, col in enumerate(["dependency", "edjefa", "edjefe"]):
+    ax = plt.subplot(3, 1, i + 1)
+    # Iterate through the poverty levels
+    for poverty_level, color in colors.items():
+        sns.kdeplot(train.loc[train["Target"] == poverty_level, col].dropna(), ax=ax, color=color,
+                    label=poverty_mapping[poverty_level])
+
+    plt.title(f"{col.capitalize()} Distribution")
+    plt.xlabel(f"{col}")
+    plt.ylabel("Density")
+
+plt.subplots_adjust(top=2)
+# plt.show()
+
+test["Target"] = np.nan
+data = train.append(test, ignore_index=True)
+
+# Exploring Label Distribution
+heads = data.loc[data["parentesco1"] == 1].copy()
+
+train_labels = data.loc[(data["Target"].notnull()) & (data["parentesco1"] == 1), ["Target", "idhogar"]]
+label_counts = train_labels["Target"].value_counts().sort_index()
+
+label_counts.plot.bar(figsize=(8, 6), color=colors.values(), edgecolor="k", linewidth=2)
+
+plt.xlabel("Poverty Level")
+plt.ylabel("Count")
+plt.xticks([x - 1 for x in poverty_mapping.keys()], list(poverty_mapping.values()), rotation=60)
+plt.title("Poverty Level Breakdown")
+# plt.show()
+
+# print(label_counts)
+
+# Addressing Wrong Labels
+# Identify Errors
+all_equal = train.groupby("idhogar")["Target"].apply(lambda x: x.nunique() == 1)
+not_equal = all_equal[all_equal != True]
+
+print(f"There are {len(not_equal)} households where the family members do not all have the same target.")
+# print(train[train["idhogar"] == not_equal.index[0]][["idhogar", "parentesco1", "Target"]])
+
+# Families without Heads of Household
+households_leader = train.groupby("idhogar")["parentesco1"].sum()
+households_no_head = train.loc[train["idhogar"].isin(households_leader[households_leader == 0].index), :]
+
+print(f"There are {households_no_head['idhogar'].nunique()} households without a head.")
+
+households_no_head_equal = households_no_head.groupby("idhogar")["Target"].apply(lambda x: x.nunique() == 1)
+
+print(f"{sum(households_no_head_equal == False)} Households with no head have different labels.")
